df497aa2bb6274c1e48c9703966bb21959818b38
Mahlckov <tiphaine.chapeau97@orange.fr>
2023-03-13 19:04:57 +0100

    utilise twig pour mes templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,15 +14,14 @@ class MainController extends AbstractController{
      * @Route("/", name="main_home")
      */
     public function home(){
-        echo 'coucou';
-        die();
+        //on déplace le code html dans un fichier twig
+        return $this->render('main/home.html.twig');
     }
 
     /**
      * @Route("/test", name="main_test")
      */
     public function test(){
-        echo 'testounet';
-        die();
+        return $this->render('main/test.html.twig');
     }
 }
